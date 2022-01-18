43dc60054c1836053ab16d63bb991cd1b0155a80
Jinho Kim <rlawlsgh8113@naver.com>
2022-01-19 00:58:40 +0900

    객체 지향 핵심 2022-01-19

---
+++ OOP_Core/src/polymorphism/VIPCustomer.java
@@ -0,0 +1,34 @@
+package polymorphism;
+
+public class VIPCustomer extends Customer{
+    private int agentID;
+    public double salesRatio;
+
+    public VIPCustomer(int customerID, String customerName, int agentID) {
+        super(customerID, customerName);
+
+        this.agentID = agentID;
+        bonusRatio = 0.05;
+        salesRatio = 0.1;
+        customerGrade = "VIP";
+    }
+
+    @Override
+    public int calcPrice(int price) {
+        bonusPoint += price * bonusRatio;
+        return price - (int)(price * salesRatio);
+    }
+
+    @Override
+    public String showCustomerInfo() {
+        return super.showCustomerInfo() + " 담당 상담원 번호는 " + agentID + "입니다";
+    }
+
+    public int getAgentID() {
+        return agentID;
+    }
+
+    public void setAgentID(int agentID) {
+        this.agentID = agentID;
+    }
+}
